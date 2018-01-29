d28f4a3623b5eff4f8e0e341ba1dccc983957d27
chadschulte <cjschulte@oakland.edu>
2018-01-29 00:00:35 -0500

    Added if/else statements depending on how many bottles are left

--- BeerSong.java
+++ BeerSong.java
@@ -7,12 +7,20 @@ public class BeerSong{
 		String word = "bottles";
 
 		while(beerNum > 0){
-			System.out.println(beerNum + " " + word + " of 
-beer on the wall, " + beerNum + " " + word + " of beer");
-			System.out.println("take one down, pass it 
-around, ");
+			System.out.println(beerNum + " " + word + " of beer on the wall, " + beerNum + " " + word + " of beer");
+			System.out.println("take one down, pass it around, ");
 
 			beerNum = beerNum - 1;
+
+			if(beerNum == 1){
+				word = "bottle";
+			}
+			if(beerNum > 0){
+				System.out.println(beerNum + " " + word + " of beer on the wall.");
+			}
+			else{
+				System.out.println("No more bottles of beer on the wall.");
+			}
 		}
 	}
 	
