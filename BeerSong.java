2ba15db88a00247a0aaf8a6f612bb8651628b9ea
chadschulte <cjschulte@oakland.edu>
2018-01-28 23:52:22 -0500

    Added while loop for first part of song

--- BeerSong.java
+++ BeerSong.java
@@ -5,6 +5,15 @@ public class BeerSong{
 	public static void Ninety_Nine_Bottles_of_Beer(){
 		int beerNum = 99;
 		String word = "bottles";
+
+		while(beerNum > 0){
+			System.out.println(beerNum + " " + word + " of 
+beer on the wall, " + beerNum + " " + word + " of beer");
+			System.out.println("take one down, pass it 
+around, ");
+
+			beerNum = beerNum - 1;
+		}
 	}
 	
 	public static void main(String[] args){
