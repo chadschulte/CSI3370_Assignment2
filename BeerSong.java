3e1d53929702289e0b9337445fd5714de4e2a310
chadschulte <cjschulte@oakland.edu>
2018-01-28 23:43:31 -0500

    Initialized beerNum and word variables

--- BeerSong.java
+++ BeerSong.java
@@ -3,7 +3,8 @@ package assignment2;
 public class BeerSong{
 
 	public static void Ninety_Nine_Bottles_of_Beer(){
-
+		int beerNum = 99;
+		String word = "bottles";
 	}
 	
 	public static void main(String[] args){
